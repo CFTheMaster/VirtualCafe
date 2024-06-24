aa5dc05b2118bd85c332c507a003a96157ee8058
Emillia <hentaimasterpad@gmail.com>
2024-06-24 20:36:30 +0200

    fix minutes to show double digits and do a proper conditional rendering

--- src/components/AboutData.vue
+++ src/components/AboutData.vue
@@ -21,17 +21,15 @@ export default {
   <WelcomeItem>
     <template #heading>Opening hours</template>
 
-    <div class="green" v-if="(isWeekend = 1)">
-      <h1>The Virtual Cafe is not open on the weekends</h1>
-    </div>
+    <div class="green" v-if="(isWeekend === 0)"><h1>The Virtual Cafe is not open on the weekends</h1></div>
     <div class="green" v-else>
       <h1>The Virtual Cafe is open between these times</h1>
       <h3>
         {{ new Date('2024-06-24' + 'T12:00:00.000Z').getHours() }}:{{
-          new Date('2024-06-24' + 'T12:00:00.000Z').getMinutes()
+          new Date('2024-06-24' + 'T12:00:00.000Z').getMinutes().toString().padStart(2, '0')
         }}
         to {{ new Date('2024-06-24' + 'T20:00:00.000Z').getHours() }}:{{
-          new Date('2024-06-24' + 'T20:00:00.000Z').getMinutes()
+          new Date('2024-06-24' + 'T20:00:00.000Z').getMinutes().toString().padStart(2, '0')
         }}
       </h3>
       Time is based on your current local time
